2f0e41bf1e27f0a0bf3e56765a58af737cdad878
Enrique Garcia <garcia@lapp.in2p3.fr>
2019-11-15 14:37:47 +0100

    impromevent of the function to be tested as suggested by issue #2

--- template_project_escape/tests/test_code_escape.py
+++ template_project_escape/tests/test_code_escape.py
@@ -7,3 +7,4 @@ from template_project_escape.code_template_escape import square_number
 def test_square_number():
     test_number = square_number(np.random.random_sample())
     assert test_number > 0
+    np.testing.assert_equal(square_number(2), 4)  # Another example of syntax for the unitary test

4a846af1069eb7bf6e88d49217b3689d975bfcd5
Enrique Garcia <40355845+garciagenrique@users.noreply.github.com>
2020-02-26 16:49:26 +0100

    Travis implementation (#1)
    * add travis and entry points
    
    * correct import in setup
    
    * take out osx from travis
    
    * include env.yml file

--- template_project_escape/code_template_escape.py
+++ template_project_escape/code_template_escape.py
@@ -2,17 +2,46 @@
 # Template module with an example for the ESCAPE project
 
 import numpy as np
+import argparse
 
 
 def square_number(number):
-    """Function that returns the square of the input number.
+    """
+    Function that returns the square of the input number.
 
     Parameters:
     -----------
-    number: int or float - number to square.
+        number: int or float
+            Number to square.
 
     Returns:
     --------
-    the square of the input number.
+        np.square(number) : numpy.int64
+            The square of the input number. The result is rounded to 3 decimal places !
     """
-    return np.square(number)
+    return np.round(np.square(number), 3)
+
+
+def main():
+    """
+    Prints the output of the square_number function, passed as an argument.
+
+    """
+    parser = argparse.ArgumentParser(description="Square the input number !")
+
+    parser.add_argument('--input_number', '-i',
+                        type=float,
+                        dest='input',
+                        help='Input number to square. Default = -7.',
+                        default=-7.
+                        )
+
+    args = parser.parse_args()
+
+    print('\n\tThe square of {} is {} !\n'.format(args.input,
+                                                  square_number(args.input)
+                                                  ))
+
+
+if __name__ == '__main__':
+    main()

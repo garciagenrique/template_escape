e04b9f7c23ef9e9fbe2bcc02f811709109166d7d
Enrique Garcia <garcia@lapp.in2p3.fr>
2020-03-02 19:41:16 +0100

    add readme for the singularity directory

--- singularity/create_singularity_image.sh
+++ singularity/create_singularity_image.sh
@@ -6,5 +6,5 @@ sudo singularity build escape_template.simg singularity_template_escape.recipe
 
 rm env_template_escape.yml
 
-echo "Container created ! To run it : >> singularity run escape_template.simg #NUMBER# "
-echo "To get help run : >> singularity run-help escape_template.simg"
+echo -e "\n\tContainer created ! To run it : >> singularity run escape_template.simg #NUMBER# "
+echo -e "\tTo get help run : >> singularity run-help escape_template.simg\n"

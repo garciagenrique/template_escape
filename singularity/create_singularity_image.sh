53c2129942b90363717c5acfaa68f3edd94450ae
Enrique Garcia <garcia@lapp.in2p3.fr>
2020-03-02 23:50:32 +0100

    resolve conflicts

--- singularity/create_singularity_image.sh
+++ singularity/create_singularity_image.sh
@@ -6,10 +6,5 @@ sudo singularity build escape_template.simg singularity_template_escape.recipe
 
 rm env_template_escape.yml
 
-<<<<<<< HEAD
 echo -e "\n\tContainer created ! To run it : >> singularity run escape_template.simg #NUMBER# "
 echo -e "\tTo get help run : >> singularity run-help escape_template.simg\n"
-=======
-echo "Container created ! To run it : >> singularity run escape_template.simg #NUMBER# "
-echo "To get help run : >> singularity run-help escape_template.simg"
->>>>>>> bae869328cea251fb355e2912dd7736c113ba95e

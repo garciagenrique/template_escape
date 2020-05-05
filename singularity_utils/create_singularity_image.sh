3f90b470d2b5138ae12fac20933bf1cff73d95d5
Enrique <garcia@lapp.in2p3.fr>
2020-05-06 01:58:23 +0200

    update some file s name

--- singularity_utils/create_singularity_image.sh
+++ singularity_utils/create_singularity_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cp ../env_template_escape.yml env_template_escape.yml
+cp ../environment.yml environment.yml
 
 sudo singularity build escape_template.simg singularity_template_escape.recipe
 
-rm env_template_escape.yml
+rm environment.yml
 
 echo -e "\n\tContainer created ! To run it : >> singularity run escape_template.simg #NUMBER# "
 echo -e "\tTo get help run : >> singularity run-help escape_template.simg\n"

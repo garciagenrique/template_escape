a2380e026985ec138a9c685b2305c4acbdcd83a5
Enrique Garcia <garcia@lapp.in2p3.fr>
2020-08-04 10:45:14 +0200

    Update doc and relicense

--- .zenodoci/upload_new_version_deposit.py
+++ .zenodoci/upload_new_version_deposit.py
@@ -3,7 +3,7 @@ import os
 import json
 import argparse
 from distutils.util import strtobool
-from .zenodoapi import ZenodoAPI
+from zenodoapi import ZenodoAPI
 
 parser = argparse.ArgumentParser(description="Upload a new version of an existing deposit to Zenodo")
 
